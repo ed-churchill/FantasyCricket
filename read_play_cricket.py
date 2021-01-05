16c9578cfdc364ced11d176101fef7dc6ef11a0a
Ed Churchill <ed.churchill00@gmail.com>
2021-01-05 20:56:22 +0000

    Start writing smaller function that only gets the batting scorecard

--- read_play_cricket.py
+++ read_play_cricket.py
@@ -3,15 +3,18 @@ update a spreadsheet accordingly"""
 
 import pandas as pd
 
-def scorecard_to_df(scorecard_url):
-    """Function that reads a scorecard from PlayCricket.com and converts it to a Pandas dataframe
+def batting_to_df(match_url):
+    """Function that reads the batting scorecard from PlayCricket.com and converts it to a Pandas dataframe
 
-    :param str scorecard_url: The URL of the scorecard on PlayCricket.com
+    :param str match_url: The URL of the scorecard on PlayCricket.com
     """
 
-    # Find the first batting scorecard
-    data = pd.read_html(scorecard_url)
+    # Fetch all the tables on the page
+    tables = pd.read_html(match_url)
 
+    
+
+print(scorecard_to_df("https://uniofwarwick.play-cricket.com/website/results/4055612"))
 
 
 
