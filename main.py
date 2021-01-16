1673eaf90f7475e87b3ce42b911b29b7bb54cf6a
Ed Churchill <ed.churchill00@gmail.com>
2021-01-16 19:28:03 +0000

    Test on different scorecards

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import read_play_cricket
 
 print(read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612"))
+# print(read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612"))
+# https://mudeford.play-cricket.com/website/results/4515077
+# 'https://mudeford.play-cricket.com/website/results/3851311'
