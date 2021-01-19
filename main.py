40fea70a397e4c877a0e3f772da4187a8fe59673
Ed Churchill <ed.churchill00@gmail.com>
2021-01-19 17:39:32 +0000

    Test read_play_cricket on more links.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import read_play_cricket
 
+# Test the clean_scorecards on different links
 print(read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612"))
-# print(read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612"))
-# https://mudeford.play-cricket.com/website/results/4515077
-# 'https://mudeford.play-cricket.com/website/results/3851311'
+print(read_play_cricket.clean_scorecards('https://uniofwarwick.play-cricket.com/website/results/4052768'))
+print(read_play_cricket.clean_scorecards('https://uniofwarwick.play-cricket.com/website/results/4052769'))
+
