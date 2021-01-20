dcd480f0b1af59602df9d3661c79132f2a4dff23
Ed Churchill <ed.churchill00@gmail.com>
2021-01-20 17:26:17 +0000

    Create module to write to google sheets and add function that returns the sheet 'FantasyCricketPlayerStats'

--- write_google_sheets.py
+++ write_google_sheets.py
@@ -1,14 +1,28 @@
+"""Module containing functions to write data from Dataframes into Google Sheets"""
+
 import read_play_cricket
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Define the scope of the application
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
 
-# Add credentials to the account
-creds = ServiceAccountCredentials.from_json_keyfile_name('FantasyCricket-7a5a400ddc86.json', scope)
+def get_sheet():
+    """Function that returns the Google Sheet 'FantasyCricketPlayerStats'."""
 
-# Authorise the client sheet
-client = gspread.authorize(creds)
+    # Define the scope of the application
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/spreadsheets',
+             'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
+
+    # Add credentials to the account
+    creds = ServiceAccountCredentials.from_json_keyfile_name('FantasyCricket-7a5a400ddc86.json', scope)
+
+    # Authorise the client sheet
+    client = gspread.authorize(creds)
+
+    # Get the sheet
+    sheet = client.open('FantasyCricketPlayerStats')
+    return sheet
+
+
+week_1 = get_sheet().get_worksheet(0)
+print(week_1.cell(3, 2))
