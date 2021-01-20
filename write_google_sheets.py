b210c8a0aa81b66de1d4132e8f952c27fbb50a95
Ed Churchill <ed.churchill00@gmail.com>
2021-01-20 18:59:15 +0000

    Write logic to update the Google Sheet (needs to be changed to reduce the no. of API calls per second

--- write_google_sheets.py
+++ write_google_sheets.py
@@ -33,6 +33,52 @@ def update_stats(bat_df, bowl_df, field_df, week_number):
     :param week_number The Week Number of the sheet to be updated.
     """
 
+    # Get the relevant sheet instance
+    sheet = get_sheet().get_worksheet(week_number - 1)
 
-week_1 = get_sheet().get_worksheet(0)
-print(week_1.cell(3, 2))
+    # Update batting stats (and add one to games played of each batsman in the game)
+    for index in bat_df.index:
+
+        # Get name from dataframe
+        name = bat_df['BATSMAN'][index]
+
+        name_found = False
+
+        # Find the name in the first column of the spreasheet. If it's not found, prompt the user to manually type the
+        # name in.
+        sheet_row_count = sheet.row_count
+        for row in range(3, sheet_row_count + 3):
+            name_on_sheet = sheet.cell(row, 2).value
+
+            # Case where name is found
+            if name == name_on_sheet:
+                name_found = True
+
+                # Add one to games played
+                current_games = sheet.cell(row, 3).value
+                if current_games == '':
+                    current_games = 0
+                else:
+                    current_games = int(current_games)
+                sheet.update_cell(row, 3, current_games + 1)
+
+                # Update runs
+                # Update 4s
+                # Update 6s
+                # Update 50s
+                # Update 100s
+                # Update 150s
+                # Update 200s
+                # Update Ducks
+
+        # Case where name is not found (so name_found is still false)
+        if not name_found:
+            print(name + " was not found and the sheet wasn't updated")
+
+        # THERE ARE TOO MANY API REQUESTS PER SECOND. TO FIX THIS, I NEED TO GET THE LIST OF NAMES AS A LIST AND STORE
+        # IT IN A VARIABLE AS A LIST, INSTEAD OF REQUESTING THE VALUE ON EVERY FOR LOOP ITERATION.
+
+
+batting_df, bowling_df, fielding_df = read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612")
+update_stats(batting_df, bowling_df, fielding_df, 1)
+
