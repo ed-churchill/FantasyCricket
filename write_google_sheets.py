c17e27111b1ce2cd172eccb27aca339f562a4f33
Ed Churchill <ed.churchill00@gmail.com>
2021-01-23 15:36:22 +0000

    Reduce number of API calls and update game counter

--- write_google_sheets.py
+++ write_google_sheets.py
@@ -36,49 +36,44 @@ def update_stats(bat_df, bowl_df, field_df, week_number):
     # Get the relevant sheet instance
     sheet = get_sheet().get_worksheet(week_number - 1)
 
-    # Update batting stats (and add one to games played of each batsman in the game)
-    for index in bat_df.index:
+    # Get the list of names in the sheet
+    sheet_names = sheet.col_values(2)[2:]
 
-        # Get name from dataframe
-        name = bat_df['BATSMAN'][index]
+    # Get the list of names from the batting Dataframe
+    batsman_list = list(bat_df['BATSMAN'])
 
-        name_found = False
+    # For loop to update each player's batting stats
+    for name in batsman_list:
 
-        # Find the name in the first column of the spreasheet. If it's not found, prompt the user to manually type the
-        # name in.
-        sheet_row_count = sheet.row_count
-        for row in range(3, sheet_row_count + 3):
-            name_on_sheet = sheet.cell(row, 2).value
+        # Case where names match
+        if name in sheet_names:
+            # Row index of the name in the sheet
+            name_row_index = sheet_names.index(name) + 3
 
-            # Case where name is found
-            if name == name_on_sheet:
-                name_found = True
+        # Case where names don't match
+        else:
+            valid_name = False
+            while not valid_name:
+                # Prompt user input
+                print('The name "' + name + '" was not found on the sheet.')
+                user_input = input('Please type the correct name, as it appears in the sheet.').strip()
 
-                # Add one to games played
-                current_games = sheet.cell(row, 3).value
-                if current_games == '':
-                    current_games = 0
+                # Check if user input is valid. If it is, update the sheet
+                if user_input in sheet_names:
+                    valid_name = True
+                    name_row_index = sheet_names.index(user_input) + 3
                 else:
-                    current_games = int(current_games)
-                sheet.update_cell(row, 3, current_games + 1)
+                    print("Your input was not found on the sheet. Please try again \n")
 
-                # Update runs
-                # Update 4s
-                # Update 6s
-                # Update 50s
-                # Update 100s
-                # Update 150s
-                # Update 200s
-                # Update Ducks
+        # Add one to the player's game count
+        sheet.update_cell(name_row_index, 3, 1)
 
-        # Case where name is not found (so name_found is still false)
-        if not name_found:
-            print(name + " was not found and the sheet wasn't updated")
+        # Create list of stats that will be used to update the sheet. The list will be of the form:
+        # [RUNS, 4s, 6s, 50s, 100s, 150s, 200s, DUCKS]
 
-        # THERE ARE TOO MANY API REQUESTS PER SECOND. TO FIX THIS, I NEED TO GET THE LIST OF NAMES AS A LIST AND STORE
-        # IT IN A VARIABLE AS A LIST, INSTEAD OF REQUESTING THE VALUE ON EVERY FOR LOOP ITERATION.
+        print(name + "'s stats have been successfully updated \n")
 
 
-batting_df, bowling_df, fielding_df = read_play_cricket.clean_scorecards("https://uniofwarwick.play-cricket.com/website/results/4055612")
+batting_df, bowling_df, fielding_df = read_play_cricket.clean_scorecards(
+    "https://uniofwarwick.play-cricket.com/website/results/4055612")
 update_stats(batting_df, bowling_df, fielding_df, 1)
-
