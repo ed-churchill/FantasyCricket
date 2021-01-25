27c8d35238a90f3a0628ecbd2a63a53e37859e92
Ed Churchill <ed.churchill00@gmail.com>
2021-01-25 19:11:01 +0000

    Further testing

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import read_play_cricket
 import write_google_sheets
 
 test_links = ['https://uniofwarwick.play-cricket.com/website/results/4055612',
-              'https://uniofwarwick.play-cricket.com/website/results/4052768',
               'https://uniofwarwick.play-cricket.com/website/results/4052769']
 
 # Test the script that updates the stats on the test_links
