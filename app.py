bfa28775e76d29ac367bbcc547429a75ee6cf80a
Ed Churchill <ed.churchill00@gmail.com>
2021-01-30 18:48:56 +0000

    Set up Flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # download data here
+
+    table_data = [("Ed Churchill", "Warwick"),
+                ("Kacper", "Imperial"),
+                ("Bronson Carrasco", "Essex"),
+                ("Michael Roper-Cowley", "Bristol"),
+                ("Alan Philip", "LSE"),
+                ("James Morrison", "Oxford")]
+    return render_template("index.html", users=table_data)
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+
+if __name__ == "__main__":
+    app.run(debug)
